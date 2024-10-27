5aae4192f152feaa8ae9d96c7d5020fcd900e88f
Tyuiu.DolganovAA@outlook.com <Tyuiu.DolganovAA@outlook.com>
2024-10-27 18:29:27 +0300

    Add project files.

---
+++ Tyuiu.DolganovAA.Sprint3.Task0.V2.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.DolganovAA.Sprint3.Task0.V2.Lib
+{
+    public class DataService : ISprint3Task0V2
+    {
+        public double GetMultiplySeries(int startValue, int stopValue)
+        {
+            double sinValue = Math.Sin(1);
+            double sinPowerNeg7 = Math.Pow(sinValue, -7);
+
+            double result = 1.0;
+
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                double term = Math.Pow(i / sinPowerNeg7, -2);
+                result *= term;
+            }
+
+            return Math.Round(result, 3);
+
+        }
+    }
+}
---
+++ Tyuiu.DolganovAA.Sprint3.Task0.V2.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+using Tyuiu.DolganovAA.Sprint3.Task0.V2.Lib;
+
+namespace Tyuiu.DolganovAA.Sprint3.Task0.V2.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService service = new DataService();
+            Assert.AreEqual(0.021, service.GetMultiplySeries(1, 9));
+        }
+    }
+}
